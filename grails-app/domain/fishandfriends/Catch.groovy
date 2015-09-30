e84877267a6de27bd0661f36f97e2252be4245b6
Florent BERBIE <florent-berbie@hotmail.fr>
2015-09-30 08:45:57 +0200

    Ajout du modèle Catch et de sa classe test associée #16

---
+++ grails-app/domain/fishandfriends/Catch.groovy
@@ -0,0 +1,19 @@
+package fishandfriends
+
+/**
+ * Catch for an only fish by a fishing man.
+ */
+class Catch {
+
+    /** Date of the catch */
+    Date date
+
+    /** Weight of the fish catching */
+    Float weight
+
+    /** Size of the fish catching */
+    Float size
+
+    static constraints = {
+    }
+}
