9d7811801efe3611df65909dd7c2ac3abcddc5f8
Florent BERBIE <florent-berbie@hotmail.fr>
2015-09-30 08:37:33 +0200

    Ajout du modele Fish et de sa classe test associe #16

---
+++ grails-app/domain/fishandfriends/Fish.groovy
@@ -0,0 +1,19 @@
+package fishandfriends
+
+/**
+ * Class with data according the type of fish.
+ */
+class Fish {
+
+    /** Fish name */
+    String name
+
+    /** Weight for this type of fish */
+    Float weightAverage
+
+    /** Size for this type of fish */
+    Float sizeAverage
+
+    static constraints = {
+    }
+}
