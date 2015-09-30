04a09d4d4d636a24d2c7601e83e4e87b732f9108
Florent BERBIE <florent-berbie@hotmail.fr>
2015-09-30 08:50:41 +0200

    Ajout du modele Note et de sa classe test associee #16

---
+++ grails-app/domain/fishandfriends/Note.groovy
@@ -0,0 +1,19 @@
+package fishandfriends
+
+/**
+ * Class to attribute a note to a <b>FisherMan</b> and a <b>FishingArea</b>.
+ */
+class Note {
+
+    /** Fishing man link to the Note */
+    FishingMan fishingMan
+
+    /** Fishing man link to the Note */
+    FishingArea fishingArea
+
+    /** Value of the note */
+    int value
+
+    static constraints = {
+    }
+}
