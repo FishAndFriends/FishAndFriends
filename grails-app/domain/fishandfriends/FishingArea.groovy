fe0499fbe914d2187d876215a96b37f6b9214db1
Florent BERBIE <florent-berbie@hotmail.fr>
2015-09-30 08:29:21 +0200

    Ajout du modele FishingArea et de sa class test associee

---
+++ grails-app/domain/fishandfriends/FishingArea.groovy
@@ -0,0 +1,18 @@
+package fishandfriends
+
+/**
+ * Class for a Fishing area. A fishing area contains information on a area added
+ * by a fishing man.
+ */
+class FishingArea {
+
+    /** Name of the fishing area (e.g Garonne) */
+    String name
+
+    /** Location (e.g Toulouse)*/
+    String location
+
+    static constraints = {
+
+    }
+}
