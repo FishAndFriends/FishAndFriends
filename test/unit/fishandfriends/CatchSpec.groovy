bd4990b6ebc83260f56780ac5f52175cbb2bded9
Florent BERBIE <florent-berbie@hotmail.fr>
2015-10-01 09:13:47 +0200

    Ajout de tests unitaires et des contraintes pour "Catch" #19

--- test/unit/fishandfriends/CatchSpec.groovy
+++ test/unit/fishandfriends/CatchSpec.groovy
@@ -9,12 +9,60 @@ import spock.lang.Specification
 @TestFor(Catch)
 class CatchSpec extends Specification {
 
+    Catch fishCatch
+
     def setup() {
+        fishCatch = new Catch()
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test a valid catch"() {
+        given:"a catch"
+        fishCatch.date = catchDate
+        fishCatch.weight = weightCatch
+        fishCatch.size = sizeCatch
+
+        when:"validating the fishCatch"
+        def isValid = fishCatch.validate()
+
+        then: "the fishCatch is valid"
+        isValid == true
+
+        where:
+        catchDate               | weightCatch   | sizeCatch | fishingManCatch   | fishingAreaCatch
+        new Date(2012, 2, 12)   | 12.2          | 66        | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2015, 3, 2)    | 9             | 22        | Mock(FishingMan)  | Mock(FishingArea)
+
+
+    }
+
+    void "test an invalid catch"() {
+        given:"a catch"
+        fishCatch.date = catchDate
+        fishCatch.weight = weightCatch
+        fishCatch.size = sizeCatch
+
+        when:"validating the fishCatch"
+        def isValid = fishCatch.validate()
+
+        then: "the fishCatch is not valid"
+        isValid == false
+
+        where:
+        catchDate               | weightCatch   | sizeCatch | fishingManCatch   | fishingAreaCatch
+        null                    | 12.2          | 66        | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(4051,3,12)     | 12.2          | 66        | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2015, 3, 2)    | -2.0          | 22        | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2015, 3, 2)    | 12.2          | -1        | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2015, 3, 2)    | 0.0           | 22        | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2015, 3, 2)    | 12.2          | 0.0       | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2012, 2, 2)    | null          | 66        | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2012, 2, 2)    | 12.2          | null      | Mock(FishingMan)  | Mock(FishingArea)
+        new Date(2015, 3, 2)    | 12.2          | 0.0       | null              | Mock(FishingArea)
+        new Date(2015, 3, 2)    | 12.2          | 0.0       | Mock(FishingMan)  | null
+        new Date(2015, 3, 2)    | 12.2          | 0.0       | null              | null
+
     }
 }
--- test/unit/fishandfriends/FishingManSpec.groovy
+++ test/unit/fishandfriends/FishingManSpec.groovy
@@ -88,4 +88,3 @@ class FishingManSpec extends Specification {
 
     } */
 }
-}
