0f28ab61cbf83c3d9bc335a6de6faaec444b253f
Florent BERBIE <florent-berbie@hotmail.fr>
2015-10-09 11:06:04 +0200

    Possibility to modify informations or password #4

--- grails-app/controllers/fishandfriends/FishingManController.groovy
+++ grails-app/controllers/fishandfriends/FishingManController.groovy
@@ -48,9 +48,33 @@ class FishingManController {
         }
 
         if (params.lastnameEdit) {
-            fishingManInstance.firstname = params.lastnameEdit
+            fishingManInstance.lastname = params.lastnameEdit
         }
 
         fishingManService.insertOrUpdateFishingMan(fishingManInstance)
+        redirect action: "show", id: fishingManInstance.id
+    }
+
+    def editPassword(FishingMan fishingManInstance) {
+        if (fishingManInstance == null) {
+            notFound()
+            return
+        }
+
+        if (fishingManInstance.hasErrors()) {
+            render(view: "edit", model:[fishingManInstance: fishingManInstance])
+        }
+        def password = fishingManInstance.saltedPassword + params.oldPassword
+        password = password.encodeAsSHA1()
+
+        if (fishingManInstance.hashedPassword.equals(password)) {
+            fishingManInstance.hashedPassword =
+                    (fishingManInstance.saltedPassword + params.newPassword).encodeAsSHA1()
+            fishingManService.insertOrUpdateFishingMan(fishingManInstance)
+            redirect action: "show", id: fishingManInstance.id
+        } else {
+            errors.add(message(code: "fishandfriends.fishingMan.userNotFound"))
+        }
+
     }
 }
