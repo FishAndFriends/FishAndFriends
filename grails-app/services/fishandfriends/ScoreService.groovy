e98baa1627ee69d5f51d8d44ec600055524a588a
jochy <alexandre.dubois-2@etu.univ-tours.fr>
2015-10-15 08:46:30 +0200

    FishAndFriends-14 Score des pêcheur - Ajout JavaDoc

--- grails-app/services/fishandfriends/ScoreService.groovy
+++ grails-app/services/fishandfriends/ScoreService.groovy
@@ -5,6 +5,11 @@ import grails.transaction.Transactional
 @Transactional
 class ScoreService {
 
+    /**
+     * Method used to retrieve all scores for a fishingMan
+     * @param f : fishman
+     * @return all scores
+     */
     public ScoreObject computeScores(FishingMan f){
         ScoreObject scoreObject = new ScoreObject()
 
@@ -21,6 +26,7 @@ class ScoreService {
             }
         }
 
+        // Set object to return
         if(result?.size()>0) {
             scoreObject.nbCatch = result[0][0]
             scoreObject.averageSize = result[0][1]
