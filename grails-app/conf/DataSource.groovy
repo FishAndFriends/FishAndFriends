3926af2137575cf00767c90407ee757476816374
meGAmeS1 <florentin.lemoal@gmail.com>
2015-11-11 20:16:15 +0100

    Passage temporaire de la prod en H2 au lieu de PostGre

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -32,14 +32,15 @@ environments {
     production {
         dataSource {
             dbCreate = "create-drop"
-            driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQLDialect
-
-            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-
-            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-            username = uri.userInfo.split(":")[0]
-            password = uri.userInfo.split(":")[1]
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+//            driverClassName = "org.postgresql.Driver"
+//            dialect = org.hibernate.dialect.PostgreSQLDialect
+//
+//            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+//
+//            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+//            username = uri.userInfo.split(":")[0]
+//            password = uri.userInfo.split(":")[1]
         }
     }
 }
